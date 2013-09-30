298161d7c0ed7a536ff303c87410c50ec6561b59
Calum J. Eadie <calum@calumjeadie.com>
2013-09-30 18:21:46 +0100

    Added contact form example

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Examples and exercises from The Django Book, Apress.
 Resources
 ---------
 
+https://docs.djangoproject.com/en/dev/intro/
+
 http://www.djangobook.com/en/2.0/index.html
 
 https://github.com/jacobian/djangobook.com
---
+++ mysite/views.py
@@ -0,0 +1,35 @@
+import logging
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+
+from forms import ContactForm
+
+logger = logging.getLogger(__name__)
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # send_mail(
+            #     cd['subject'],
+            #     cd['message'],
+            #     cd.get('email', 'noreply@example.com'),
+            #     ['siteowner@example.com'],
+            # )
+            logger.info([cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),
+                ['siteowner@example.com']])
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm(
+            initial={'subject': 'I love your site!'}
+        )
+    return render(request, 'contact_form.html', {'form': form})
+
+def thanks(request):
+    return HttpResponse("Thanks!")
